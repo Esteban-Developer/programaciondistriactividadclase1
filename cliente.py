80999682cf2a5977f1c49d442dacdb2bfea2ead3
Esteban-Developer <estebancontacto.dev@gmail.com>
2026-02-20 20:45:31 -0500

    actualizacion del cliente con envio de nombre

--- cliente.py
+++ cliente.py
@@ -3,7 +3,14 @@ import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("127.0.0.1", 5000))
 
-nombre = client.recv(1024).decode()
-print(f"Hola, el estudiante conectado es: {nombre}")
+# Pedir el nombre al usuario
+nombre_estudiante = input("Ingrese su nombre: ")
+
+# Enviar el nombre al servidor
+client.sendall(nombre_estudiante.encode())
+
+# Recibir la respuesta del servidor
+respuesta = client.recv(1024).decode()
+print("Respuesta del servidor:", respuesta)
 
 client.close()
